c9a4673850ef9f9548f2ee10767cbc23f3a3cbd1
Xiaolei Qin <xiaolei_qin@outlook.com>
2024-10-19 00:47:40 -0500

    fix: path

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://orionq.github.io',
-  base: '/Portfolio',
+  base: '',
   integrations: [
     tailwind(),
     react({ experimentalReactChildren: true }),

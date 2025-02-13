75f1a30ceaaf890517fd5b9802d4e937b6c75bd5
Xiaolei Qin <xiaolei_qin@outlook.com>
2025-02-13 17:33:13 -0600

    feat: add about me section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(10px)" },
+        },
+      },
+      animation: {
+        "float-slow": "float 2s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [],
+};
